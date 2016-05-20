3b9f15f51bb0ddbb81e94601d4b7717af0a9351d
Andrew Bassford <bassford@gmail.com>
2016-05-20 10:08:28 -0700

    moving S3 call around

--- index.js
+++ index.js
@@ -17,15 +17,17 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  // publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   filesAdapter: new S3Adapter(
     "AKIAJR5A4DJSE4CUUY6Q",
     "O3pwpE+pyGgvLAuR/LB5yteKDxZEuiNFJJJ9rLeo",
     "painxchange",
     {directAccess: true}
-  )
+  ),
+  liveQuery: {
+    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  }
+
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
