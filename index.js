caba5c9cda15ebf4f21fa9cf62335f5624dc12b3
Andrew Bassford <bassford@gmail.com>
2016-05-20 11:29:24 -0700

    add file key and client key env variables

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  // publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  fileKey: process.env.FILE_KEY,
+  clientKey: process.env.CLIENT_KEY,
   filesAdapter: new S3Adapter(
     process.env.S3_KEY_ID,
     process.env.S3_SECRET_KEY,
